25febc679509f50bbe7ca1b830547eeff64e1dd4
Jorge Maroto <patoroco@gmail.com>
2016-04-24 23:29:53 +0200

    Remove jenkins, because it's more logical to run it from script

--- index.js
+++ index.js
@@ -5,34 +5,6 @@
 var dotenv = require('dotenv').config()
 var openTunnel = require('./lib/ngrok-slack').openTunnel
 
-var fs = require('fs');
-
-
-
-var spawn = require('child_process').spawn
-
-var prc = spawn('jenkins',  ['--httpPort=8081', '--ajp13Port=8010'])
-prc.stdout.setEncoding('utf8');
-
-prc.stdout.on('data', function (data) {
-  var str = data.toString()
-  var lines = str.split(/(\r?\n)/g);
-  let date = new Date().toLocaleString()
-
-
-  fs.writeFile("/tmp/ngrok-jenkins.log", date + " " + lines.join(""), function(err) {
-      if(err) {
-          return console.log(err);
-      }
-  })
-})
-
-prc.on('close', function (code) {
-    console.log('process exit code ' + code);
-})
-
-
-
 var config = {
   ngrok: {
     port: process.env.NGROK_PORT,
